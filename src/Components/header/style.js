3c93082c44068b360512963cf393b42a9cf90ebf
Adriana <adrianascosta9@gmail.com>
2023-04-11 16:04:33 -0300

    organização-arquivos-criação-header

---
+++ src/Components/header/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 6.5rem;
+    background-color: #FFFFFF;
+    font-family: 'Roboto', sans-serif;
+    gap: 40rem;
+    background-color: #008B8B;
+
+
+`
